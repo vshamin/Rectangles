0565686a3a3d9df8f451dbc3e88abf1e2616deeb
Владимир Шамин <v-shamin@mail.ru>
2020-10-13 07:01:25 +0300

    Initial Commit

---
+++ Rectangles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rectangles
+//
+//  Created by Шамин Владимир on 13.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
